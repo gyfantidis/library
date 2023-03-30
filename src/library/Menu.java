f3e36233bdbac95f0bd583a187e91d21ec0e1fa4
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-03-30 20:50:55 +0300

    First Commit

--- src/library/Menu.java
+++ src/library/Menu.java
@@ -40,4 +40,79 @@ public class Menu {
     }
 
 
+    public static Book getBook(Scanner input, boolean lending) {
+        System.out.println("Please give isbn: ");
+        String isbn = input.next();
+        System.out.println("Please give the title: ");
+        String title = input.next();
+        int copyNum = getIntInput(input, "Please give number of copies: ");
+
+        if (lending) {
+            return new LendingBook(isbn, title, copyNum);
+        } else {
+            return new SellingBook(isbn, title, copyNum);
+        }
+    }
+
+
+    public static void getAddBook(Scanner input, boolean lending, Library library) {
+        Book book = getBook(input, lending);
+        boolean result = false;
+        if (lending) {
+            result = library.addLendingBook((LendingBook) book);
+        } else {
+            result = library.addSellingBook((SellingBook) book);
+        }
+
+        if (result) {
+            System.out.println("Book was successfully added");
+        } else {
+            System.out.println("Book probably already added in library");
+        }
+    }
+
+
+    public static void lendOrSellBook(Scanner input, boolean lending, Library library) {
+        System.out.println("Please give isbn: ");
+        String isbn = input.next();
+        System.out.println("Please give member id: ");
+        String memberId = input.next();
+
+        boolean result = false;
+        String word1 = "lended";
+        String word2 = "lend";
+
+        if (lending) {
+            result = library.loanBook(isbn, memberId);
+        } else {
+            result = library.sellBook(isbn, memberId);
+            word1 = "sold";
+            word2 = "sell";
+        }
+
+        if (result) {
+            System.out.println("Book was successfully " + word1);
+        } else {
+            System.out.println("Something went wrong while trying to " + word2 + " book"
+                    + "with isbn: " + isbn + " to member with id: " + memberId);
+        }
+    }
+
+
+    public static void  returnBook(Scanner input, Library library){
+        System.out.println("Please give isbn: ");
+        String isbn = input.next();
+        System.out.println("Please give member id: ");
+        String memberId = input.next();
+
+        boolean result = library.returnBook(isbn, memberId);
+        if(result){
+            System.out.println("Book was successfully returned");
+        }
+        else{
+            System.out.println("Something went wrong while trying to return book");
+        }
+    }
+
+
 }
